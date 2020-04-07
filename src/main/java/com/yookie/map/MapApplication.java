9eedd1aaf5c8e23c656e7f05c891e27fa8d11079
Yookiely <zihang1998115@126.com>
2020-04-07 16:08:05 +0800

    注意！！此版本为打包版本，若要本地跑，请修改properties

--- src/main/java/com/yookie/map/MapApplication.java
+++ src/main/java/com/yookie/map/MapApplication.java
@@ -4,16 +4,25 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 
 @MapperScan("com.yookie.map.mapper")
 @SpringBootApplication
 @EnableCaching
-public class MapApplication {
+public class MapApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MapApplication.class, args);
     }
+
+    @Override//为了打包springboot项目
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
+
 
 }
--- src/main/java/com/yookie/map/controller/NormalIndexController.kt
+++ src/main/java/com/yookie/map/controller/NormalIndexController.kt
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpServletRequest
 
 
 @RestController
@@ -16,9 +18,10 @@ class NormalIndexController {
     val normalIndexService : NormalIndexService? = null
 
 
-    @GetMapping("/normal/{param}/{date}/{hour}")
-    fun getNormal(@PathVariable("date") date : String,@PathVariable("param") param : String,@PathVariable("hour") hour : String) : List<List<NormalIndex>>{
-        return normalIndexService!!.getNormalIndexByDate(param, date, hour)
+    @RequestMapping("/normal")
+    fun getNormal( request : HttpServletRequest) : List<List<NormalIndex>>{
+        return normalIndexService!!.getNormalIndexByDate(request.getParameter("param"), request.getParameter("date"), request.getParameter("hour"))
+
     }
 
 
